5c36a454bd7e2e7a5a63e4959d041b67707d0181
dnsf27 <danasofiagomez@gmail.com>
2026-04-04 17:12:11 -0500

    Implementar encontrarElementoMenor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -99,9 +99,13 @@ public class Workshop {
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+        int menor = arreglo[0];
+        for (int i = 1; i < arreglo.length; i++) {
+            if (arreglo[i] < menor) {
+                menor = arreglo[i];
+            }
+        }
+        return menor;
     }
 
     // Método que busca un elemento en un arreglo
