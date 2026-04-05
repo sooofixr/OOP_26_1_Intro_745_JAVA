10fa2f68f284a4d6c01d490d46c075df4842e7f1
dnsf27 <danasofiagomez@gmail.com>
2026-04-05 12:17:24 -0500

    Implementar convertirAMinusculas(cadena)

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -244,9 +244,7 @@ public class Workshop {
 
     // Método que convierte una cadena a minúsculas
     public String convertirAMinusculas(String cadena) {
-        // TODO: Implementar el método para convertir una cadena a minúsculas.
-        // Ejemplo: Si cadena = "HELLO", el resultado debería ser "hello".
-        return "";
+        return cadena.toLowerCase();
     }
 
     // Método que reemplaza una subcadena en una cadena por otra subcadena
