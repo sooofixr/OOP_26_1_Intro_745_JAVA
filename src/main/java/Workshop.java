4abe7865e02710801039f2d676241c81d942b56c
dnsf27 <danasofiagomez@gmail.com>
2026-04-05 11:20:38 -0500

    Implementar contarCaracteres(cadena)

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -202,9 +202,7 @@ public class Workshop {
 
     // Método que cuenta los caracteres en una cadena
     public int contarCaracteres(String cadena) {
-        // TODO: Implementar el método para contar el número de caracteres en una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
-        return 0;
+        return cadena.length();
     }
 
     // Método que invierte una cadena
