811664961fdf9187b5dc5307fd2cf6b055438174
dnsf27 <danasofiagomez@gmail.com>
2026-04-05 12:22:27 -0500

    Implementar reemplazarSubcadena(cadena, vieja, nueva)

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -249,9 +249,7 @@ public class Workshop {
 
     // Método que reemplaza una subcadena en una cadena por otra subcadena
     public String reemplazarSubcadena(String cadena, String antiguaSubcadena, String nuevaSubcadena) {
-        // TODO: Implementar el método para reemplazar una subcadena en una cadena por otra subcadena.
-        // Ejemplo: Si cadena = "Hello Java", antiguaSubcadena = "Java", y nuevaSubcadena = "world", el resultado debería ser "Hello world".
-        return "";
+        return cadena.replace(antiguaSubcadena, nuevaSubcadena);
     }
 
     // Método que busca una subcadena en una cadena y retorna su índice
